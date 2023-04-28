4eca3ba92019523783fa9119195251219c402604
Alette Ng Xin Ling <xinlingng@Alettes-MacBook-Air.local>
2023-04-28 14:02:04 -0400

    starter interface cont'd

---
+++ src/run/mainrun.java
@@ -0,0 +1,14 @@
+
+public class mainrun {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        
+        //create a user 
+        
+        //the user calls add photo 
+        
+            
+    }
+
+}
